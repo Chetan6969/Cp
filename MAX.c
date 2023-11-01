6db0b39b04032decba2717a94245dd52bb270c15
Chetan6969 <chetansen2004@gmail.com>
2023-11-01 13:15:58 +0530

    Maximum program is added

---
+++ MAX.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+
+	printf("Enter a = \n");
+	scanf(" %d",&a);
+	printf("Enter b = \n");
+	scanf(" %d ",&b);
+
+	if(a>b)
+	{
+	printf(" A is greather ");
+	}
+	else
+	{
+	printf("B is greather \n");
+	}
+	getch();
+}
